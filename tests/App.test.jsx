3eb166f557a5d5baf53fba42a020343f661c7f62
Angel Ivanov <angel.iwanov@gmail.com>
2024-07-29 12:19:56 +0300

    adding vitest

---
+++ tests/App.test.jsx
@@ -0,0 +1,12 @@
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import { describe, expect, test } from 'vitest';
+import App from '../src/App';
+
+describe('App', () => {
+  test('renders headline', () => {
+    render(<App />);
+    expect(screen.getByText('Phonebook')).toBeVisible();
+    expect(screen.getByText('Numbers')).toBeVisible();
+  });
+});
