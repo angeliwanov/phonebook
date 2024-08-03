edd6e385d470562034a204af35c28e92e5f05f5a
Angel Ivanov <angel.iwanov@gmail.com>
2024-08-03 09:03:52 +0300

    renaming app.spec.js

--- e2e-tests/App.spec.js
+++ e2e-tests/App.spec.js
@@ -1,8 +1,10 @@
-const { test, expect } = require('@playwright/test');
+const { test, describe, expect } = require('@playwright/test');
 
-test('front page can be opened', async ({ page }) => {
-  await page.goto('/');
-  await expect(page.getByText('Phonebook')).toBeVisible();
-  await expect(page.getByText('Add a number')).toBeVisible();
-  await expect(page.getByText('Numbers')).toBeVisible();
+describe('app', () => {
+  test('front page can be opened', async ({ page }) => {
+    await page.goto('/');
+    await expect(page.getByText('Phonebook')).toBeVisible();
+    await expect(page.getByText('Add a number')).toBeVisible();
+    await expect(page.getByText('Numbers')).toBeVisible();
+  });
 });
