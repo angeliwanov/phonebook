7477b113c96b91d3a4467909b1c7c3ae4d70616a
Angel Ivanov <angel.iwanov@gmail.com>
2024-08-03 09:01:55 +0300

    playwright fix issue

--- e2e-tests/App.spec.js
+++ e2e-tests/App.spec.js
@@ -1,10 +1,8 @@
-const { test, describe, expect } = require('@playwright/test');
+const { test, expect } = require('@playwright/test');
 
-describe('App', () => {
-  test('front page can be opened', async ({ page }) => {
-    await page.goto('/');
-    await expect(page.getByText('Phonebook')).toBeVisible();
-    await expect(page.getByText('Add a number')).toBeVisible();
-    await expect(page.getByText('Numbers')).toBeVisible();
-  });
+test('front page can be opened', async ({ page }) => {
+  await page.goto('/');
+  await expect(page.getByText('Phonebook')).toBeVisible();
+  await expect(page.getByText('Add a number')).toBeVisible();
+  await expect(page.getByText('Numbers')).toBeVisible();
 });
