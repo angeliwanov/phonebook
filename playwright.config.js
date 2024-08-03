c2d4b183a956eeebe2a26ad41e4aa57e8a26e6f5
Angel Ivanov <angel.iwanov@gmail.com>
2024-08-03 16:25:17 +0300

    --headed

--- playwright.config.js
+++ playwright.config.js
@@ -28,7 +28,7 @@ module.exports = defineConfig({
     baseURL: 'http://127.0.0.1:3001',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on',
+    trace: 'on-first-retry',
   },
 
   /* Configure projects for major browsers */
